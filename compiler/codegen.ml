0035499569cceb0c04f55afe7fbb184d3b267c6b
Mutsuha Asada <me@momee.mt>
2024-08-02 03:05:30 +0900

    add: wat codegen

---
+++ compiler/codegen.ml
@@ -0,0 +1,41 @@
+open Ast
+open Runtime.Instructions
+open Runtime.Modules
+
+let rec codegen_expr expr =
+  match expr with
+  | IntLit n -> Instr (I32Const (Int32 n))
+  | Plus (left, right) ->
+      Block [ codegen_expr left; codegen_expr right; Instr I32Add ]
+  | Minus (left, right) ->
+      Block [ codegen_expr left; codegen_expr right; Instr I32Sub ]
+  | Times (left, right) ->
+      Block [ codegen_expr left; codegen_expr right; Instr I32Mul ]
+  | Div (left, right) ->
+      Block [ codegen_expr left; codegen_expr right; Instr I32DivS ]
+
+let codegen ast =
+  let start_func =
+    {
+      name = "_start";
+      params = [];
+      results = [];
+      body = [ codegen_expr ast; Instr (Call "proc_exit") ];
+    }
+  in
+  let module_ =
+    {
+      imports =
+        [
+          {
+            module_name = "wasi_snapshot_preview1";
+            name = "proc_exit";
+            desc = FuncImport ("proc_exit", [ I32 ], []);
+          };
+        ];
+      exports = [ { name = "_start"; desc = FuncExport "_start" } ];
+      funcs = [ start_func ];
+      memory = Some { min_pages = 1; max_pages = None };
+    }
+  in
+  string_of_module module_
