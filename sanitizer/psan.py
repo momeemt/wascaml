7282f70e1d6f3d98b15736e46d40b457d43af51f
Mutsuha Asada <me@momee.mt>
2024-08-13 12:13:13 +0900

    fix: psan list format

--- sanitizer/psan.py
+++ sanitizer/psan.py
@@ -1,16 +1,19 @@
 import fileinput
 import sys
 
-arrs = []
+arr_addr = []
 for line in fileinput.input():
-    # [PSan : list_length] => [a1, a2, a3, ..., an] format
+    # [PSan : list_length] => addr [a1, a2, a3, ..., an] format
+    # i.g. [PSan : list_length] => 1234 [1, 2, 3]
     line = line.strip()
-    if line.startswith("[PSan : list_length] => ["):
+    if line.startswith("[PSan : list_length] => "):
+        data = line[len("[PSan : list_length] => "):]
+        addr = data.split(" ")[0]
         arr = []
-        for i in line.split(" => [")[1].split(", "):
-            arr.append(int(i.replace("]", "")))
-        arrs.append(arr)
+        for a in data.split("[")[1].split("]")[0].split(","):
+            arr.append(int(a))
+        arr_addr.append((arr, addr))
 
-for i in range(len(arrs) - 1):
-    if arrs[i] == arrs[i + 1]:
-        print(f"[PSan]: You should not call list_length against the same list! -> {arrs[i]} / {arrs[i+1]}", file=sys.stderr)
+for i in range(len(arr_addr) - 1):
+    if arr_addr[i][0] == arr_addr[i + 1][0]:
+        print(f"[PSan]: You should not call list_length against the same list! -> {arr_addr[i][0]} / {arr_addr[i+1][0]}", file=sys.stderr)
